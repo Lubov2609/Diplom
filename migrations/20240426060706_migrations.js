f365a9e7639fdbc6bff26df04ba383c5d741ad0a
Luba Skladnik <luba260902@gmail.com>
2024-05-07 11:43:38 +0300

    Переделаны миграции с  внешними ключами + сделаны сиды

---
+++ migrations/20240426060706_migrations.js
@@ -0,0 +1,62 @@
+exports.up = function(knex) {
+    return knex.schema
+        .createTable("years", table => {
+            table.increments().primary();
+            table.integer("year_name").notNullable();
+            table.timestamps(true, true);
+        })
+        .createTable("roles", table => {
+            table.increments().primary();
+            table.string("role_name",255).notNullable();
+            table.timestamps(true, true);
+        })
+        .createTable("users", table => {
+            table.increments().primary();
+            table.string("user_fio",255).notNullable();
+            table.text("login").notNullable();
+            table.text("password").notNullable();
+            table.text("email").notNullable();
+            table.timestamps(true, true);
+            table.integer("year_id").references('id').inTable('years');
+            table.integer("role_id").references('id').inTable('roles');
+        })
+        .createTable("groups", table => {
+            table.increments().primary();
+            table.string("group_name",4).notNullable();
+            table.timestamps(true, true);
+            table.integer("year_id").references('id').inTable('years');
+        })
+        .createTable("students", table => {
+            table.increments().primary();
+            table.string("student_fio",150).notNullable();
+            table.decimal("student_gpa").notNullable();
+            table.text("student_pz",150).notNullable();
+            table.text("student_tz",150).notNullable();
+            table.timestamps(true, true);
+            table.integer("group_id").references('id').inTable('groups');
+        })
+        .createTable("docs", table => {
+            table.increments().primary();
+            table.string("doc_name",50).notNullable();
+            table.string("doc_link",255).notNullable();
+            table.timestamp(true, true);
+            table.integer("year_id").references('id').inTable('years');
+        })
+        .createTable("protocols", table => {
+            table.increments().primary();
+            table.integer("user_id").references('id').inTable('users');
+            table.integer("student_id").references('id').inTable('students');
+            table.integer("list_id").notNullable();
+            table.timestamps(true, true);
+        });
+
+
+};
+
+
+
+
+exports.down = function(knex) {
+    return knex.schema.dropTable("users").dropTable("roles").dropTable("students").dropTable("docs").dropTable("protocols").dropTable("years").dropTable("groups");
+
+};
--- routes/yearsRoute.js
+++ routes/yearsRoute.js
@@ -4,8 +4,8 @@ const yearsController = require("../controllers/yearsController");
 
 router.route("/years/").get(yearsController.getAll);
 router.route("/years/:id").post(yearsController.create);
-router.route("/years/:id/year").get(yearsController.getById);
-router.route("/years/:id/edit").put(yearsController.update);
+router.route("/years/:id").get(yearsController.getById);
+router.route("/years/:id").put(yearsController.update);
 router.route("/years/:id").delete(yearsController.delete);
 
 module.exports = router;
